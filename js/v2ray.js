534a3719a4887491f77a2594100f68bdea28b1bb
V2 Ray <admin@v2ray.com>
2015-10-26 14:57:17 +0000

    use className

--- js/v2ray.js
+++ js/v2ray.js
@@ -4,18 +4,18 @@
 var NavBar = React.createClass({
   render: function() {
     return (
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#v2ray-navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#v2ray-navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="https://v2ray.com/">V2Ray</a>
+          <a className="navbar-brand" href="https://v2ray.com/">V2Ray</a>
         </div>
-        <div class="collapse navbar-collapse" id="v2ray-navbar">
-          <ul class="nav navbar-nav">
+        <div className="collapse navbar-collapse" id="v2ray-navbar">
+          <ul className="nav navbar-nav">
             <li><a href="#">About</a></li>
           </ul>
         </div>
