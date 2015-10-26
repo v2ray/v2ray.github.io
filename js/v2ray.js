9a9edac7bbc0e0fcb65a9b15b57f69e537881f7c
V2 Ray <admin@v2ray.com>
2015-10-26 17:35:26 +0000

    update

--- js/v2ray.js
+++ js/v2ray.js
@@ -1,5 +1,7 @@
 /* global React */
 /* global ReactDOM */
+/* global jQuery */
+/* global marked */
 
 var NavBar = React.createClass({
   render: function() {
@@ -46,4 +48,61 @@ var Footer = React.createClass({
 ReactDOM.render(
   <Footer />,
   document.getElementById('footer')
-)
+);
+
+var Content = React.createClass({
+  render: function() {
+    return (
+      <header className="jumbotron">
+        <h1>V2Ray 项目</h1>
+        <p>一个免费的跨平台工具，可以助你加密网络流量，绕过网络供应商的干扰。</p>
+        <p><a className="btn btn-primary btn-large" href="https://github.com/v2ray/v2ray-core/releases">下载</a></p>
+      </header>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Content />,
+  jQuery('#content')[0]
+);
+
+function Article(title, contentFile) {
+  this.title = title;
+  this.contentFile = contentFile;
+  this.content = null;
+}
+
+Article.prototype.loadContent = function(callback) {
+  var self = this;
+  jQuery.ajax({
+      url: self.contentFile,
+      type: 'get',
+      success: function(data, status, xhr) {
+        callback(data);
+      }
+    });
+};
+
+Article.prototype.getContent = function(callback) {
+  var self = this;
+  if (self.content) {
+    callback(self.content);
+    return;
+  }
+  
+  self.loadContent(function(data) {
+    self.content = data;
+    callback(data);
+  });
+};
+
+Article.prototype.getMarkedContent = function(callback) {
+  this.getContent(function(data) {
+    callback(marked(data));
+  });
+};
+
+var articles = {
+  'guide-zh-cn': new Article('用户指引', 'guide.md')
+};
